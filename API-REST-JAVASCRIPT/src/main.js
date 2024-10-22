ecc899bb5852b581bf8911333ebcbe2f41a4f7f4
GaRIEN <gabrielhuaman683@gmail.com>
2024-10-22 14:19:11 -0500

    avance de carousel de peliculas

--- API-REST-JAVASCRIPT/src/main.js
+++ API-REST-JAVASCRIPT/src/main.js
@@ -1,6 +1,46 @@
 import { API_KEY } from "./secrets.mjs";
+
+
 document.addEventListener('DOMContentLoaded',()=>{
+  const carousel = document.getElementById('carousel');
+const images = carousel.querySelectorAll('img');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+// images.forEach((img) => {
+//   img.addEventListener('dragstart', (e) => e.preventDefault());
+// });
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  carousel.classList.add('active');
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+  carousel.classList.remove('active');
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+  carousel.classList.remove('active');
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;  
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2; 
+  carousel.scrollLeft = scrollLeft - walk;
+});
+
+  
+
     getTrendingMoviesPreview();
+
 })
 
 async function getTrendingMoviesPreview() {
@@ -9,16 +49,26 @@ async function getTrendingMoviesPreview() {
     );
     const data = await res.json();
     const movies = data.results;
-    const containerpeliculas = document.getElementById("show-peliculas");
+    console.log(movies)
+    const containerpeliculas = document.getElementById("carousel");
     movies.forEach((movie) => {
       const moviehtml = `
-            <div class="show__peliculas-pelicula">
+            <a href="" class="d-inline-block text-decoration-none">
               <figure>
-                <img src="https://image.tmdb.org/t/p/w300${movie.poster_path}" alt="${movie.title}">
+                <img
+                  src="https://image.tmdb.org/t/p/w200${movie.poster_path}"
+                  alt="${movie.title}"
+                  class="rounded"
+                />
               </figure>
-              <h3 class="fs-6 mt-3 text-center text-light">${movie.title}</h3>
-            </div>
+              <div class="series-informacion">
+                <h3 class="fs-6 text-center text-light">${movie.title}</h3>
+                <p class="text-center">${movie.release_date}</p>
+              </div>
+            </a>
+
           `;
       containerpeliculas.insertAdjacentHTML("beforeend", moviehtml);
     });
   }
+
