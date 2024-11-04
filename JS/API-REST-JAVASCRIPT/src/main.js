996ca609223bfe988167d70f5d8238c3af4d2273
GaRIEN <gabrielhuaman683@gmail.com>
2024-11-03 21:16:58 -0500

    listado de generos de peliculas

--- JS/API-REST-JAVASCRIPT/src/main.js
+++ JS/API-REST-JAVASCRIPT/src/main.js
@@ -1,58 +1,56 @@
 import { API_KEY } from "./secrets.mjs";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.getElementById("carousel");
+  const images = carousel.querySelectorAll("img");
 
-document.addEventListener('DOMContentLoaded',()=>{
-  const carousel = document.getElementById('carousel');
-const images = carousel.querySelectorAll('img');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  // images.forEach((img) => {
+  //   img.addEventListener('dragstart', (e) => e.preventDefault());
+  // });
 
-let isDown = false;
-let startX;
-let scrollLeft;
-// images.forEach((img) => {
-//   img.addEventListener('dragstart', (e) => e.preventDefault());
-// });
+  carousel.addEventListener("mousedown", (e) => {
+    isDown = true;
+    carousel.classList.add("active");
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+  });
 
-carousel.addEventListener('mousedown', (e) => {
-  isDown = true;
-  carousel.classList.add('active');
-  startX = e.pageX - carousel.offsetLeft;
-  scrollLeft = carousel.scrollLeft;
+  carousel.addEventListener("mouseleave", () => {
+    isDown = false;
+    carousel.classList.remove("active");
+  });
+
+  carousel.addEventListener("mouseup", () => {
+    isDown = false;
+    carousel.classList.remove("active");
+  });
+
+  carousel.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2;
+    carousel.scrollLeft = scrollLeft - walk;
+  });
+
+  getTrendingMoviesPreview();
+  getCategoriasMovies();
 });
 
-carousel.addEventListener('mouseleave', () => {
-  isDown = false;
-  carousel.classList.remove('active');
-});
-
-carousel.addEventListener('mouseup', () => {
-  isDown = false;
-  carousel.classList.remove('active');
-});
-
-carousel.addEventListener('mousemove', (e) => {
-  if (!isDown) return;  
-  e.preventDefault();
-  const x = e.pageX - carousel.offsetLeft;
-  const walk = (x - startX) * 2; 
-  carousel.scrollLeft = scrollLeft - walk;
-});
-
-  
-
-    getTrendingMoviesPreview();
-
-})
-
+//solicitud de peliculas que estan en tend son por dias
 async function getTrendingMoviesPreview() {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
-    );
-    const data = await res.json();
-    const movies = data.results;
-    console.log(movies)
-    const containerpeliculas = document.getElementById("carousel");
-    movies.forEach((movie) => {
-      const moviehtml = `
+  const res = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+  );
+  const data = await res.json();
+  const movies = data.results;
+  console.log(movies);
+  const containerpeliculas = document.getElementById("carousel");
+  movies.forEach((movie) => {
+    const moviehtml = `
             <a href="" class="d-inline-block text-decoration-none">
               <figure>
                 <img
@@ -68,7 +66,42 @@ async function getTrendingMoviesPreview() {
             </a>
 
           `;
-      containerpeliculas.insertAdjacentHTML("beforeend", moviehtml);
-    });
-  }
+    containerpeliculas.insertAdjacentHTML("beforeend", moviehtml);
+  });
+}
 
+//solicitud de peliculas que estan en tend son por dias
+async function getCategoriasMovies() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY
+  );
+  const categorias = await res.json();
+  const genero = categorias.genres;
+
+  const generopeliculas = document.getElementById("generopeliculas");
+  const cantidad = Math.ceil(genero.length / 2);
+  const columna1 = genero.slice(0, cantidad);
+  const columna2 = genero.slice(cantidad);
+  const column1Html = `
+  <div class="col-6">
+    ${columna1
+      .map(
+        (genre) =>
+          `<li><a class="dropdown-item text-white" href="#">${genre.name}</a></li>`
+      )
+      .join("")}
+  </div>
+`;
+const column2Html = `
+<div class="col-6">
+  ${columna2
+    .map(
+      (genre) =>
+        `<li><a class="dropdown-item text-white" href="#">${genre.name}</a></li>`
+    )
+    .join("")}
+</div>
+`;
+generopeliculas.innerHTML = column1Html +  column2Html;
+
+}
