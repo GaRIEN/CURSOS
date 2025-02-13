e4cbb9acff9c07aac226a83ab356e6f4e7c02162
GaRIEN <gabrielhuaman683@gmail.com>
2025-02-12 23:53:14 -0500

    sintaxis del curso de php

---
+++ php/sintaxis/index.php
@@ -0,0 +1,17 @@
+<?php
+//para imprimir
+echo "hola , este es mi primer comentario en php <br>";
+
+$nombre="gabriel";
+$apellido="huaman";
+
+echo "yo me llamo ".$nombre ." ".$apellido."<br>";
+
+// variables
+
+$numero=8;
+
+//constante
+
+define("dunero_pi",3.14);
+echo dunero_pi;
