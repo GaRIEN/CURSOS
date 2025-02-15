a70d15de67aef3cb5efcc7229447ef3091ac9823
GaRIEN <gabrielhuaman683@gmail.com>
2025-02-15 16:50:41 -0500

    for y foreach

---
+++ php/arreglos-funciones-estruc/for.php
@@ -0,0 +1,14 @@
+<?php
+$michis = array (
+    "MichiJuan",
+    "MichiBerto",
+    "Mr.Michi",
+    "MichiMouse",
+    "PepeMichi",
+    "MichiMan",
+    "MichiMe",
+);
+for ($i=0; $i <= count($michis) - 1 ; $i++) {     
+    echo $i . "- " . $michis[$i] . "\n";
+}
+?>
