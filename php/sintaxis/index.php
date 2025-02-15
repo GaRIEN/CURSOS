beea1cc3e31668c6564df75bf426f82aaa72944f
GaRIEN <gabrielhuaman68364gmail.com>
2025-02-15 14:50:12 -0500

    clases de operadores matematicos

--- php/sintaxis/index.php
+++ php/sintaxis/index.php
@@ -15,3 +15,31 @@ $numero=8;
 
 define("dunero_pi",3.14);
 echo dunero_pi;
+
+// tipos de datos
+        /*numericos: integer, float,double
+        cadena :char string
+        boleanos: true , falso
+        */
+    
+
+
+// operadores logicos
+
+    /*
+    Y=&&
+    O=||
+    NO=!
+
+    */
+
+//Operadores Aritméticos
+    /* Adición ⇒ +
+    Sustracción ⇒ -
+    Multiplicación ⇒ *
+    División ⇒ /
+    Modulo ⇒ % ⇒ Se usa para conocer el residuo de una división ⇒ $a % $b
+    Potenciación ⇒ ** ⇒ $a ** $b
+    Identidad ⇒ Sirve para convertir un string a un int o float, según sea el caso ⇒ + ⇒ +$a
+    Negación ⇒ Convierte un numero positivo a negativo ⇒ -$a
+    */
