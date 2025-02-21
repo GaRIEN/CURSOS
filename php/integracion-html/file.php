41bc8a67776f158e37c2a26078672e0cb42e04c9
GaRIEN <gabrielhuaman683@gmail.com>
2025-02-21 18:23:07 -0500

    clase de formulario file

---
+++ php/integracion-html/file.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "<pre>";
+    var_dump($_FILES);
+    echo "</pre>";
+
+    $basename=$_FILES["image"]["name"];
+    $image = $_FILES["image"]["tmp_name"];
+    $ruta_a_subir= "./images/$basename";
+    move_uploaded_file($image,$ruta_a_subir);
+
+    echo $basename;
+
--- php/integracion-html/formulario.php
+++ php/integracion-html/formulario.php
@@ -6,13 +6,35 @@
     <title>formulario get</title>
 </head>
 <body>
-    <form action="./server.php" method="get">
+    <!-- <form action="./server.php" method="get">
         <label for="nombre">nombre</label>
         <input type="text" name="nombre" id="nombre">
         <label for="edad">edad</label>
         <input type="text" name="edad" id="edad">
 
         <button type="submit">enviar</button>
-    </form>
+    </form> -->
+
+
+    <!-- <form action="./post.php" method="post">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" id="nombre">
+        <label for="edad">edad</label>
+        <input type="text" name="edad" id="edad">
+
+        <button type="submit">enviar</button>
+    </form>  -->
+
+<!-- PARA IMAGENES-->
+
+    <form action="./file.php" method="post" enctype="multipart/form-data">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" id="nombre">
+        <label for="image">edad</label>
+        <input type="file" name="image" id="image">
+
+        <button type="submit">enviar</button>
+    </form> 
+
 </body>
 </html> 
