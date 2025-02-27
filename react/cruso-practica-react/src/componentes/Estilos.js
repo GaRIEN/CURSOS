278a5a0584f9821219f914f75110af2f005eb86a
GaRIEN <gabrielhuaman683@gmail.com>
2025-02-27 12:57:44 -0500

    styles en react

---
+++ react/cruso-practica-react/src/componentes/Estilos.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Estilos.css"
+
+function Estilos(){
+    return(
+        <>
+        <section className='estilos'>
+
+        <h2>Estilos CSS en React</h2>
+        <h3 className='bg-react'>estilos en hojas css</h3>
+        <h3 className='bg-react' style={{borderRadius:"2.5rem", margin:"1rem"}}>estilos en linea</h3>
+
+        </section>
+        </>
+    )
+}
+
+export {Estilos}
--- react/cruso-practica-react/src/componentes/Formularios.js
+++ react/cruso-practica-react/src/componentes/Formularios.js
@@ -1,23 +1,113 @@
 import React, { useState } from "react";
 
-export default function Formularios() {
-  const [nombre, setNombre] = useState("");
-  const [sabor, setSabor] = useState("");
-  const [lenguaje, setLenguaje] = useState("");
+// export default function Formularios() {
+//   const [nombre, setNombre] = useState("");
+//   const [sabor, setSabor] = useState("");
+//   const [lenguaje, setLenguaje] = useState("");
+//   const [termionos, setTerminos] = useState(false);
 
+
+//   const handleSubmit =(e)=>{
+//     e.preventDefault();
+//     console.log("se envio el formulario")
+//   }
+
+//   return (
+//     <>
+//       <h2>Formularios</h2>
+//       <form onSubmit={handleSubmit}>
+
+//         <label htmlFor="nombre">Nombre</label>
+//         <input
+//           type="text"
+//           id="nombre"
+//           name="nombre"
+//           value={nombre}
+//           onChange={(e) => setNombre(e.target.value)}
+//         />
+
+//         <p> elige tu sabor favorito</p>
+//         <input
+//           type="radio"
+//           id="piña"
+//           name="sabor"
+//           value="piña"
+//           onChange={(e) => setSabor(e.target.value)}
+//         />
+//         <label htmlFor="">piña</label>
+//         <input
+//           type="radio"
+//           id="platano"
+//           name="sabor"
+//           value="platano"
+//           onChange={(e) => setSabor(e.target.value)}
+//           defaultChecked
+
+//         />
+//         <label htmlFor="">platano</label>
+
+//         <p>elije tu lenguaje favorito</p>
+//         <select name="lenguaje" onChange={(e)=>setLenguaje(e.target.value)} defaultValue="" >
+//             <option value="js">javascript</option>
+//             <option value="php">php</option>
+//             <option value="py">python</option>
+//             <option value="java">java</option>
+//             <option value="go">go</option>
+//         </select>
+
+//         <p>CHECKBOX</p>
+//         <input type="checkbox" id="terminos" name="terminos" onChange={(e)=>setTerminos(e.target.checked)} />
+//         <label htmlFor="terminos">Acepto los terminos y condiciones</label>
+//         <br />
+
+//         <input type="submit" />
+        
+//       </form>
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+
+//     </>
+//   );
+// }
+
+export default function Formularios() {
+  const [form, setForm] = useState({});
+  
+
+
+  const handleChange =e=>{
+    //del formulario que tienes ya , me traiga el name y le asigne el value 
+    setForm({
+      ...form,[e.target.name]:e.target.value
+    })
+  }
+  const handleChecked =(e)=>{
+    setForm({
+      ...form,[e.target.name]:e.target.checked
+    })
+  }
+  const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log("se envio el formulario")
+      }
 
   return (
     <>
       <h2>Formularios</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <label htmlFor="nombre">Nombre</label>
         <input
           type="text"
           id="nombre"
           name="nombre"
-          value={nombre}
-          onChange={(e) => setNombre(e.target.value)}
+          value={form.nombre}
+          onChange={handleChange}
         />
 
         <p> elige tu sabor favorito</p>
@@ -26,7 +116,7 @@ export default function Formularios() {
           id="piña"
           name="sabor"
           value="piña"
-          onChange={(e) => setSabor(e.target.value)}
+          onChange={handleChange}
         />
         <label htmlFor="">piña</label>
         <input
@@ -34,20 +124,27 @@ export default function Formularios() {
           id="platano"
           name="sabor"
           value="platano"
-          onChange={(e) => setSabor(e.target.value)}
+          onChange={handleChange}
           defaultChecked
 
         />
         <label htmlFor="">platano</label>
 
         <p>elije tu lenguaje favorito</p>
-        <select name="lenguaje" onChange={(e)=>setLenguaje(e.target.value)} defaultValue="" >
+        <select name="lenguaje" onChange={handleChange} defaultValue="" >
             <option value="js">javascript</option>
             <option value="php">php</option>
             <option value="py">python</option>
             <option value="java">java</option>
             <option value="go">go</option>
         </select>
+
+        <p>CHECKBOX</p>
+        <input type="checkbox" id="terminos" name="terminos" onChange={handleChecked} />
+        <label htmlFor="terminos">Acepto los terminos y condiciones</label>
+        <br />
+
+        <input type="submit" />
         
       </form>
       <br />
