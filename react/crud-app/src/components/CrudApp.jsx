35b62049bd5b2657cb586d7149dd94fb8cbbbb44
GaRIEN <gabrielhuaman683@gmail.com>
2025-03-07 11:17:30 -0500

    commit initial

---
+++ react/crud-app/src/components/CrudApp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// const initialDb =[
+// {
+//     id:1,
+//     name:"seiya",
+//     constellation: "Pegaso"
+// },
+// {
+//     id:2,
+//     name:"Shiryu",
+//     constellation: "Dragón"
+// },
+// ];
+
+const CrudApp =()=>{
+    return(
+        <div>
+            <h2>CRUD APP</h2>
+        </div>
+    )
+}
+
+export default CrudApp;
--- react/cruso-practica-react/src/componentes/Estilos.js
+++ react/cruso-practica-react/src/componentes/Estilos.js
@@ -16,3 +16,4 @@ function Estilos(){
 }
 
 export {Estilos}
+
