b5409eeb4699806c6bba8aa074b2f17a61b1c6a2
GaRIEN <gabrielhuaman683@gmail.com>
2025-03-07 22:07:10 -0500

    avance de la interfaz

--- react/crud-app/src/components/CrudApp.jsx
+++ react/crud-app/src/components/CrudApp.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+import { useState } from 'react';
 
-// const initialDb =[
-// {
-//     id:1,
-//     name:"seiya",
-//     constellation: "Pegaso"
-// },
-// {
-//     id:2,
-//     name:"Shiryu",
-//     constellation: "Dragón"
-// },
-// ];
+const initialDb =[
+{
+    id:1,
+    name:"seiya",
+    constellation: "Pegaso"
+},
+{
+    id:2,
+    name:"Shiryu",
+    constellation: "Dragón"
+},
+];
 
 const CrudApp =()=>{
+    const [db, setDb] = useState(initialDb)
     return(
         <div>
             <h2>CRUD APP</h2>
+            <CrudForm/>
+            <CrudTable data={db}/>
         </div>
     )
 }
