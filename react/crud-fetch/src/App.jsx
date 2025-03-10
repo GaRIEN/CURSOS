7fb83b584c88efd3baa44f9c256fad718c13c690
GaRIEN <gabrielhuaman68364gmail.com>
2025-03-10 14:57:05 -0500

    configuracion de helpers para las peticiones de la crud

---
+++ react/crud-fetch/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import CrudApi from './components/CrudApi'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black max-auto">
+      <CrudApi />
+    </div>
+  );
+}
+
+export default App;
+
