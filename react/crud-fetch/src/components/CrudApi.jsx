a3ca54eb93e744d0d4e2e7ad0208e01194011ab1
GaRIEN <gabrielhuaman683@gmail.com>
2025-03-16 04:15:45 -0500

    error en variable

--- react/crud-fetch/src/components/CrudApi.jsx
+++ react/crud-fetch/src/components/CrudApi.jsx
@@ -45,11 +45,11 @@ const CrudApi = () => {
     api.post(url, options).then((res) => {
       console.log("los resltados 1");
       console.log(res);
-      if (!result.err) {
+      if (!res.err) {
 
         // setDb([...db, result]);
       } else {
-        setError(result);
+        setError(res);
       }
       console.log("dentro del psot");
       console.log(res);
