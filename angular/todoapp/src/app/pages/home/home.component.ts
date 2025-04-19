3d04343e1fb2b07d7f2d8832d697e4de4237ff0a
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-19 14:33:36 -0500

    creacion de tareas en el home

--- angular/todoapp/src/app/pages/home/home.component.ts
+++ angular/todoapp/src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  tasks = signal(['instalar angular', 'crear componentes']);
 
+  handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    //traemos lo que tenemos y agreamo lo nuevo
+    this.tasks.update((tasks) => [...tasks, target.value]);
+  }
+
+  deletetask(id: number) {
+    this.tasks.update((tasks) => tasks.filter((task, index) => index !== id));
+  }
 }
