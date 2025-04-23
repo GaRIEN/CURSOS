b296d19f3c52517556ef3d78f056eacdeb401f07
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-22 20:37:00 -0500

    avance de store

--- angular/store/src/app/app.component.ts
+++ angular/store/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule],
+  standalone: true,
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-  title = 'store';
+  title = 'ventas ';
 }
--- angular/store/src/app/app.routes.ts
+++ angular/store/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListComponent } from '../app/domains/products/pages/list/list.component';
+
+export const routes: Routes = [{ path: '', component: ListComponent }];
