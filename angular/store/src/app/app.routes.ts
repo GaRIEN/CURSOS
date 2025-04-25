d0537ab30a98c5b9754bf0c3a0cdf8c1dd19a348
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-24 23:53:16 -0500

    ciclo de vida de los componentes

--- angular/store/src/app/app.routes.ts
+++ angular/store/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from '../app/domains/products/pages/list/list.component';
+import { AboutComponent } from './domains/info/pages/about/about.component';
 
-export const routes: Routes = [{ path: '', component: ListComponent }];
+export const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'about', component: AboutComponent },
+];
--- angular/store/src/app/domains/products/pages/list/list.component.ts
+++ angular/store/src/app/domains/products/pages/list/list.component.ts
@@ -4,16 +4,42 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  standalone:true,
-  imports: [CommonModule , ProductComponent],
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  imgProduct: { id: number; title: string; price: number; img: string }[] = [
+    {
+      id: 1,
+      title: 'product 1',
+      price: 250,
+      img: 'https://picsum.photos/640/640?r=23',
+    },
 
-  fromChild(event:string){
-    console.log("llego al padre");
+    {
+      id: 2,
+      title: 'product 2',
+      price: 200,
+      img: 'https://picsum.photos/640/640?r=20',
+    },
+    {
+      id: 3,
+      title: 'product 3',
+      price: 50,
+      img: 'https://picsum.photos/640/640?r=21',
+    },
+    {
+      id: 4,
+      title: 'product 4',
+      price: 450,
+      img: 'https://picsum.photos/640/640?r=28',
+    },
+  ];
+
+  fromChild(event: string) {
+    console.log('llego al padre');
     console.log(event);
   }
-
 }
