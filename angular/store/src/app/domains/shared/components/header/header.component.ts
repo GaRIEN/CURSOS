d42276119ec85fd8c316508562c2017c5bbb3fba
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-29 01:31:52 -0500

    mostrando modal del carrito

--- angular/store/src/app/domains/shared/components/header/header.component.ts
+++ angular/store/src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  viewCard=signal(true);
+
+  viewCardHandler() {
+    this.viewCard.update(prev => !prev);
+
+  }
 
 }
