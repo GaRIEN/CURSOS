c28c5a11420c16d20e72295855072b888c45bf32
GaRIEN <gabrielhuaman683@gmail.com>
2025-05-14 00:51:37 -0500

    agregando el curso de c# nuevamente por error

---
+++ C#/poo/poo/Models/SuperHeroe.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo.Models
+{
+    class Superheroe:Heroe
+    {
+        // Atributos o propiedades
+        //creamos una variable priva para manejar el encapsulamiento
+        private string _NombreT;
+        //estp es un ejemplo de encapsulamiento 
+        public override string Nombre {
+            get
+            {
+                return _NombreT;
+            } // Trim quita los espacios en blanco al inicio y al final 
+            set
+            {
+                _NombreT = value.Trim();
+            }
+        }
+        public string IdentidadSecreta { get; set; }
+        public string PoderPrincipal { get; set; }
+        public int Edad { get; set; }
+        public bool EsVengador { get; set; }
+        public List<SuperPoder> poderes { get; set; } // Lista de poderes
+
+        //otro ejemplo de encapsulamiento
+        public string NombreEEntidadSecreta
+        {
+            get
+            {
+                return $"{Nombre} es ({IdentidadSecreta})";
+            }
+        }
+
+        //constructor
+        public Superheroe()
+        {
+            poderes = new List<SuperPoder>();
+            EsVengador = false;
+        }
+        public string UsarPoder()
+        {
+            //foreach(var poder in poderes)
+            //{
+            //    Console.WriteLine($"{Nombre} Usando el poder: {poder.Nombre}");
+            //}
+            StringBuilder sb = new StringBuilder();
+            foreach (var poder in poderes)
+            {
+                sb.AppendLine($"{NombreEEntidadSecreta} Usando el poder: {poder.Nombre}");
+            }
+            return sb.ToString();
+            ;
+        }
+
+        public override string salvarelmundo()
+        {
+            return $"{Nombre} salvará el mundo";
+        }
+
+        public override string salvarTierra()
+        {
+           /* return base.salvarTierra();*/ // Llama al método de la clase base
+            // Sobreescribimos el método de la clase base
+            return $"{Nombre} salvará la tierra desde superheroe";
+        }
+    }
+}
---
+++ C#/poo/poo/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Text;
+using poo.Models;
+
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad de volar y razonar";
+poderVolar.Nivel = NivelPoder.Medio;
+
+var poderSuperFuerza = new SuperPoder();
+poderSuperFuerza.Nombre = "Super Fuerza";
+poderSuperFuerza.Descripcion = "Capacidad de levantar objetos pesados";
+poderSuperFuerza.Nivel = NivelPoder.Alto;
+
+
+
+var superheroe1 = new Superheroe();
+superheroe1.Nombre = "   gabriel         ";
+superheroe1.IdentidadSecreta = "gabo";
+superheroe1.PoderPrincipal = "telepatia";
+superheroe1.Edad = 25;
+superheroe1.EsVengador = true;
+List<SuperPoder> gabriel = new List<SuperPoder>();
+gabriel.Add(poderVolar);
+gabriel.Add(poderSuperFuerza);
+superheroe1.poderes = gabriel;
+
+//para un aniheroe
+var wolvering = new AntiHeroe();
+wolvering.Nombre = "   luis         ";
+wolvering.IdentidadSecreta = "luisa";
+wolvering.PoderPrincipal = "no se ";
+wolvering.Edad = 50;
+wolvering.EsVengador = false;
+
+List<SuperPoder> poderesWolvering = new List<SuperPoder>();
+poderesWolvering.Add(poderVolar);
+poderesWolvering.Add(poderSuperFuerza);
+wolvering.poderes = poderesWolvering;
+
+
+string user=  superheroe1.UsarPoder();
+Console.WriteLine(user);
+Console.WriteLine(superheroe1.salvarelmundo());
+Console.WriteLine(superheroe1.salvarTierra());
+
+string resultWolvering = wolvering.UsarPoder();
+Console.WriteLine(resultWolvering);
+Console.WriteLine(wolvering.Accion("robar"));
+
+
+enum NivelPoder
+{
+    Bajo,
+    Medio,
+    Alto
+}
