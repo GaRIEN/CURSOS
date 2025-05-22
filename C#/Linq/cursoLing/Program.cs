3c23a0f366965d9a5f9c08131cf7b8b18057dd7f
GaRIEN <gabrielhuaman683@gmail.com>
2025-05-21 22:18:14 -0500

    curso nuevo

---
+++ C#/Linq/cursoLing/Program.cs
@@ -0,0 +1,12 @@
+﻿var frutas = new string[] { "Sandia", "Fresa", "Mango", "Mango de azúcar", "Mango Tomy" };
+
+var MangoList = frutas.Where(fruta => fruta.StartsWith("Mango")).ToList();
+//var MangoList = frutas.Where(fruta => fruta.StartsWith("Mango")).ToList();
+
+
+//foreach (var mango in MangoList)
+//{
+//    Console.WriteLine(mango);
+//}
+
+MangoList.ForEach(mango => Console.WriteLine(mango));
