2a3ae5e719477ef7b5a8fcf2f39afb315c4f01d2
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-02 21:47:59 -0500

    creacion del proyecto

---
+++ C#/Projectef/pojectef/Models/Tarea.cs
@@ -0,0 +1,18 @@
+﻿namespace pojectef.Models
+{
+    public class Tarea
+    {
+        public Guid TareaId { get; set; }
+        public Guid CategoriaId { get; set; } // Foreign key to Categoria
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public prioridad Prioridad { get; set; } // Enum for priority
+        public DateTime FechaCreacion { get; set; }
+        public bool Completada { get; set; }
+        // Navigation property for related Categoria
+        public virtual Categoria Categoria { get; set; }
+    }
+
+    public enum prioridad { Alta, Media, Baja }
+
+}
---
+++ C#/Projectef/pojectef/TareaContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using pojectef.Models;
+
+namespace pojectef
+{
+    public class TareaContext : DbContext
+
+    {
+        public DbSet<Categoria> categorias { get; set; }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public TareaContext(DbContextOptions<TareaContext> options) : base(options)
+        {
+        }
+
+    }
+}
