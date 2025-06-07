48b819432e04c8871b1731851ab343f398d761ba
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-07 00:26:13 -0500

    actualizar y delete tareas

--- C#/Projectef/pojectef/Program.cs
+++ C#/Projectef/pojectef/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using pojectef;
 using pojectef.Models;
+using System.Runtime.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -79,5 +80,76 @@ app.MapGet("/api/tareas", async ([FromServices] TareaContext dbContext) =>
 });
 
 
+app.MapGet("/api/tareas/{id}", async ([FromServices] TareaContext dbContext, Guid id) =>
+{
+    var tarea = await dbContext.Tareas
+        .Include(p => p.Categoria)
+        .Where(t => t.TareaId == id)
+        .Select(t => new
+        {
+            t.TareaId,
+            t.Titulo,
+            t.Descripcion,
+            t.Prioridad,
+            t.FechaCreacion,
+            t.Completada,
+            Categoria = new
+            {
+                t.Categoria.CategoriaId,
+                t.Categoria.Nombre
+            }
+        })
+        .FirstOrDefaultAsync();
+    return tarea != null ? Results.Ok(tarea) : Results.NotFound();
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareaContext dbContext, [FromBody] Tarea tarea) =>
+{
+    if (tarea == null)
+    {
+        return Results.BadRequest("Tarea no puede ser nula.");
+    }
+
+    tarea.TareaId = Guid.NewGuid(); // Generar nuevo ID
+    tarea.FechaCreacion = DateTime.UtcNow; // Asignar fecha de creación actual
+    await dbContext.Tareas.AddAsync(tarea);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareaContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = await dbContext.Tareas.FindAsync(id);
+    if (tareaActual == null)
+    {
+        return Results.NotFound();
+    }
+
+    tareaActual.CategoriaId = tarea.CategoriaId;
+    tareaActual.Titulo = tarea.Titulo;
+    tareaActual.Prioridad = tarea.Prioridad;
+    tareaActual.Descripcion = tarea.Descripcion;
+
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareaContext dbContext, [FromRoute] Guid id) =>
+{
+    var tarea = await dbContext.Tareas.FindAsync(id);
+    if (tarea == null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Tareas.Remove(tarea);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+
+
 
 app.Run();
+
