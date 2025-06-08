94787786281a9670b46363174411ba426364e753
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-08 16:53:54 -0500

    clase de atributos para verbos Http

--- C#/WebApi/WebApi/Controllers/WeatherForecastController.cs
+++ C#/WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -11,23 +11,48 @@ namespace WebApi.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private static List<WeatherForecast> listWeartherForecast = new List<WeatherForecast>();
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
+            if (listWeartherForecast == null || !listWeartherForecast.Any())
+            {
+                listWeartherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToList();
+            }
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return listWeartherForecast;
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast data)
+        {
+            listWeartherForecast.Add(data);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= listWeartherForecast.Count)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound("Index out of range");
+            }
+            listWeartherForecast.RemoveAt(index);
+            return Ok();
         }
     }
 }
