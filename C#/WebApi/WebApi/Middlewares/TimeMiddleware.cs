3328b76a24b312975324554bfa609b0deea6abb9
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-10 22:38:00 -0500

    creacion de middleware

---
+++ C#/WebApi/WebApi/Middlewares/TimeMiddleware.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+    public class TimeMiddleware 
+    {
+        private readonly RequestDelegate _next;
+        public TimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Log the current time
+            var currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine($"Current Time: {currentTime}");
+            // Call the next middleware in the pipeline
+            await _next(context);
+    }
+
+}
+
