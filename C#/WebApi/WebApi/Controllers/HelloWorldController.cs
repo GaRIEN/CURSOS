a434025e67965fa29f653bc4cd118e7a9cd48990
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-11 19:38:25 -0500

    clase : inyeccion de dependencias

---
+++ C#/WebApi/WebApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly IHelloWorldService helloWorldService;
+
+        public HelloWorldController(IHelloWorldService helloWorldService)
+        {
+            this.helloWorldService = helloWorldService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(helloWorldService.GetHelloWorldMessage());
+        }
+    }
+}
---
+++ C#/WebApi/WebApi/Services/HelloWorldService.cs
@@ -0,0 +1,15 @@
+﻿
+public class HelloWorldService : IHelloWorldService
+{
+
+    public string GetHelloWorldMessage()
+    {
+        return "Hello, World!";
+    }
+
+}
+
+public interface IHelloWorldService
+{
+    string GetHelloWorldMessage();
+}
