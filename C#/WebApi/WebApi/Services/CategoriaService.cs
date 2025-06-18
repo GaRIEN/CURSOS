d672b7dc824bc7658cf253ac097f2d3c83412cf7
GaRIEN <gabrielhuaman683@gmail.com>
2025-06-17 20:50:33 -0500

    creando servicio de categorias y tareas

---
+++ C#/WebApi/WebApi/Services/CategoriaService.cs
@@ -0,0 +1,63 @@
+﻿using webapi;
+using webapi.Models;
+namespace webapi.Services;
+
+public class CategoriaService : ICategoriaService
+{
+    TareasContext _context;
+    public CategoriaService(TareasContext bdcontext)
+    {
+        _context = bdcontext;
+    }
+    public IEnumerable<Categoria> GetCategorias()
+    {
+        return _context.Categorias.ToList();
+    }
+
+    public async Task SaveCategoria(Categoria categoria)
+    {
+        if (categoria.CategoriaId == Guid.Empty)
+        {
+            categoria.CategoriaId = Guid.NewGuid();
+            _context.Categorias.Add(categoria);
+        }
+        else
+        {
+            _context.Categorias.Update(categoria);
+        }
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateCategoria(Guid id, Categoria categoriadata)
+    {
+        var categoriaActual = await _context.Categorias.FindAsync(id);
+        if (categoriaActual != null)
+        {
+            categoriaActual.Nombre = categoriadata.Nombre;
+            categoriaActual.Descripcion = categoriadata.Descripcion;
+            categoriaActual.Peso = categoriadata.Peso;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteCategoria(Guid id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+        if (categoria != null)
+        {
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+
+}
+
+public interface ICategoriaService
+{
+    IEnumerable<Categoria> GetCategorias();
+    Task SaveCategoria(Categoria categoria);
+    Task UpdateCategoria(Guid id, Categoria categoriadata);
+
+    Task DeleteCategoria(Guid id);
+}
---
+++ C#/WebApi/WebApi/Services/TareaService.cs
@@ -0,0 +1,62 @@
+﻿
+using webapi;
+using webapi.Models;
+
+namespace WebApi.Services
+{
+    public class TareaService : ITareaService
+    {
+        TareasContext _context;
+        public TareaService(TareasContext bdcontext)
+        {
+            _context = bdcontext;
+        }
+        public IEnumerable<Tarea> GetTareas()
+        {
+            return _context.Tareas.ToList();
+        }
+
+        public async Task SaveTarea(Tarea tarea)
+        {
+            if (tarea.TareaId == Guid.Empty)
+            {
+                tarea.TareaId = Guid.NewGuid();
+                _context.Tareas.Add(tarea);
+            }
+            else
+            {
+                _context.Tareas.Update(tarea);
+            }
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateTarea(Guid id, Tarea tareadata)
+        {
+            var tareaActual = await _context.Tareas.FindAsync(id);
+            if (tareaActual != null)
+            {
+                tareaActual.Titulo = tareadata.Titulo;
+                tareaActual.Descripcion = tareadata.Descripcion;
+                tareaActual.PrioridadTarea = tareadata.PrioridadTarea;
+                tareaActual.FechaCreacion = tareadata.FechaCreacion;
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteTarea(Guid id)
+        {
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea != null)
+            {
+                _context.Tareas.Remove(tarea);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
+public interface ITareaService
+{
+    IEnumerable<Tarea> GetTareas();
+    Task SaveTarea(Tarea tarea);
+    Task UpdateTarea(Guid id, Tarea tareadata);
+    Task DeleteTarea(Guid id);
+}
