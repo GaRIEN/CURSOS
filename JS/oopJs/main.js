25fb894c23b94f2d5af083e564ae4fafbe5632cd
GaRIEN <gabrielhuaman683@gmail.com>
2025-09-26 00:17:48 -0500

    curso de oop

---
+++ JS/oopJs/main.js
@@ -0,0 +1,13 @@
+const user = {
+    name : "gabriel",
+    age:27,
+    showFullName(){
+        return "my name is " + this.name
+    }
+}
+
+//  function showFullName(){
+//     return "garien"
+//  }
+console.log(user);
+console.log(user.showFullName());
