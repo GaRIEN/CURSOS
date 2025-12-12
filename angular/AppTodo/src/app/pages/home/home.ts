6e4ecfc66194e929ceecf2b8d814249a684fa43a
GaRIEN <gabrielhuaman683@gmail.com>
2025-12-12 00:10:53 -0500

    avance de agregar y eliminar tareas

--- angular/AppTodo/src/app/pages/home/home.ts
+++ angular/AppTodo/src/app/pages/home/home.ts
@@ -11,15 +11,53 @@ export class Home {
 
   tasks = signal([
     {
+      id:"1",
       name: "aprender angular",
-      description:"aprender curso basico luego dominaras",
-      status:"pendiente"
+      description: "aprender curso basico luego dominaras",
+      status: "pendiente"
     },
     {
+      id: "2",
       name: "aprender ingles",
       description: "Empezar desde 0",
       status: "proceso"
     },
 
   ])
+
+  //PARA MODAL 
+  showModal = signal(false);
+  // Valores reactivos (signals)
+  newName = signal('');
+  newDescription = signal('');
+  newStatus = signal('Pendiente');
+
+  saveTask() {
+    
+    this.tasks.update(t => [
+      ...t,
+      {
+        id: this.generateId(),
+        name: this.newName(),
+        description: this.newDescription(),
+        status: this.newStatus()
+      }
+    ]);
+    // Reset
+    this.newName.set('');
+    this.newDescription.set('');
+    this.newStatus.set('Pendiente');
+
+    this.showModal.set(false);
+  }
+
+  generateId() {
+    return crypto.randomUUID();
+  }
+
+  deleteTask(id: string) {
+    this.tasks.update(t => t.filter(item => item.id !== id));
+  }
+
+
 }
