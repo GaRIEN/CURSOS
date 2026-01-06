f02cb373fed17b40622e68fb60f1949bf1c410c9
GaRIEN <gabrielhuaman683@gmail.com>
2026-01-05 23:02:27 -0500

    configuracion de tailwind y estructura del curso

--- angular/tiendaOnline/src/app/app.routes.ts
+++ angular/tiendaOnline/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {List} from './domains/products/pages/list/list'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: List }
+];
