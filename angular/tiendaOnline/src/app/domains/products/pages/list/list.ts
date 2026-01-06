2ffa31691b00e5defc2f283e4f05e8bbe66678b1
GaRIEN <gabrielhuaman683@gmail.com>
2026-01-05 23:54:38 -0500

    Input comunicacion de padre a hijo, pasamos datos del padre al hijo

--- angular/tiendaOnline/src/app/domains/products/pages/list/list.ts
+++ angular/tiendaOnline/src/app/domains/products/pages/list/list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import{Product} from '../../components/product/product'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule,Product],
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
